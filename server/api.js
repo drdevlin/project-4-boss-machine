4d0c3d8da54a2c4293b74be28cb3b325a29f5b59
drdevlin <drdevlin@fastmail.com>
2020-12-31 10:56:12 -0500

    Mount routers

--- server/api.js
+++ server/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// Mount routers
+const ideasRouter = require('./routers/ideas');
+const meetingsRouter = require('./routers/meetings');
+const minionsRouter = require('./routers/minions');
 
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
+apiRouter.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
