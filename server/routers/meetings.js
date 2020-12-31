f74130d49aee56a9b2b75ff7ac41a300fd580147
drdevlin <drdevlin@fastmail.com>
2020-12-31 14:18:12 -0500

    Add meeting actions to router

--- server/routers/meetings.js
+++ server/routers/meetings.js
@@ -2,5 +2,26 @@ const express = require('express');
 const meetingsRouter = express.Router();
 
 
+// Require in actions
+const {
+  validateBody,
+  sendResponse,
+} = require('../actions/genericActions');
+
+const {
+  getAllMeetings,
+  postMeeting,
+  deleteAllMeetings,
+} = require('../actions/meetingsActions');
+
+// Routes
+meetingsRouter.get('/', getAllMeetings);
+
+meetingsRouter.post('/', postMeeting);
+
+meetingsRouter.delete('/', deleteAllMeetings);
+
+meetingsRouter.use(sendResponse);
+
 
 module.exports = meetingsRouter;
