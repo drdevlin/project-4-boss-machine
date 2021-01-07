e27e2911ac52185a705a1204ecff98d4036a91bb
drdevlin <drdevlin@fastmail.com>
2021-01-07 12:07:19 -0500

    Create ideas actions and add to router

---
+++ server/actions/ideasActions.js
@@ -0,0 +1,58 @@
+const { 
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require('../db/db');
+
+// GET all ideas
+const getAllIdeas = (req, res, next) => {
+  const ideas = getAllFromDatabase('ideas');
+  req.payload = ideas;
+  next();
+}
+
+// POST new idea
+const postNewIdea = (req, res, next) => {
+  const proposedIdea = req.body;
+  const createdIdea = addToDatabase('ideas', proposedIdea);
+  req.payload = createdIdea;
+  res.status(201);
+  next();
+}
+
+// GET one idea
+const getOneIdea = (req, res, next) => {
+  const id = req.params.ideaId;
+  const idea = getFromDatabaseById('ideas', id);
+  if (!idea) res.status(404).send();
+  req.payload = idea;
+  next();
+}
+
+// PUT one idea
+const updateOneIdea = (req, res, next) => {
+  const proposedUpdate = req.body;
+  const updatedIdea = updateInstanceInDatabase('ideas', proposedUpdate);
+  if (!updatedIdea) res.status(404).send();
+  req.payload = updatedIdea;
+  next();
+}
+
+// DELETE one idea
+const deleteOneIdea = (req, res, next) => {
+  const id = req.params.ideaId;
+  const deleted = deleteFromDatabasebyId('ideas', id);
+  if (!deleted) res.status(404).send();
+  res.status(204);
+  next();
+}
+
+module.exports = {
+  getAllIdeas,
+  postNewIdea,
+  getOneIdea,
+  updateOneIdea,
+  deleteOneIdea,
+}
--- server/routers/ideas.js
+++ server/routers/ideas.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const ideasRouter = express.Router();
 
+// Require in actions
+const {
+  sendResponse,
+} = require('../actions/genericActions');
 
+const checkMillionDollarIdea = require('../actions/checkMillionDollarIdea');
+
+const {
+  getAllIdeas,
+  postNewIdea,
+  getOneIdea,
+  updateOneIdea,
+  deleteOneIdea,
+} = require('../actions/ideasActions');
+
+// Routes
+ideasRouter.get('/', getAllIdeas, sendResponse);
+ideasRouter.post('/', checkMillionDollarIdea, postNewIdea, sendResponse);
+ideasRouter.get('/:ideaId', getOneIdea, sendResponse);
+ideasRouter.put('/:ideaId', updateOneIdea, sendResponse);
+ideasRouter.delete('/:ideaId', deleteOneIdea, sendResponse);
 
 module.exports = ideasRouter;
